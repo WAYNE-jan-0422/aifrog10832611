513b68e0d35dc1762617576e8f34e1fc98272156
WAYNE-jan-0422 <76466333+WAYNE-jan-0422@users.noreply.github.com>
2021-05-11 15:31:11 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://wayne-jan-0422.github.io/aifrog10832611/model.json';
 
 
 function preload() {
